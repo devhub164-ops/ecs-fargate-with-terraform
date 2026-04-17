612cab6d95ec6ef1c430da80a342e0f3f8dbc592
Mike <mikemonchu@gmail.com>
2026-04-17 22:55:33 +0200

    update to trigger a build

--- docker/program.py
+++ docker/program.py
@@ -4,7 +4,7 @@ def main():
     print("Hello World from ECS Fargate!")
     print("Sleeping for 20 seconds...")
     time.sleep(20)
-    print("Waking up! Goodbye!")
+    print("Waking up! Goodbye, good demo!!")
 
 if __name__ == "__main__":
     main()
